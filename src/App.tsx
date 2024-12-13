1e6da14951c0b34dd4ad8b04a33c7156ad85b0bc
Birat <thapabirat95@gmail.com>
2024-12-13 21:21:28 +0545

    project and state connection using react-redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-
-
-
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import { Provider } from "react-redux"
+import store from "./store/store"
 function App() {
 
 
   return (
-    <>
-      <div className="text-3xl font-bold underline">Hello world</div>
-    </>
+    <Provider store={store}>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<h1>This is home Page</h1>} />
+      <Route path="/register" element={<h1>This is Register Page</h1>} />
+      <Route path="/login" element={<h1>This is Login Page</h1>} />
+    </Routes>
+    </BrowserRouter>
+    </Provider>
   )
 }
 
